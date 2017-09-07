cd911d9e271a604fbe1b10a4db3cb76bddefc548
David Chobotský <david.chobotsky@gmail.com>
2017-09-07 10:07:44 +0200

    first write test version

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # keboola-ex-eWay-api-data-connector
 
+## Install locally
+
+clone git and call composer install
 
 
-## Docs
+## Docs & Links
 
 Keboola Custom Science
 https://developers.keboola.com/extend/custom-science/quick-start/
@@ -11,3 +14,8 @@ https://developers.keboola.com/extend/custom-science/php/
 
 eWay CRM API
 https://kb.eway-crm.com/dokumentace/6-doplnky/6-7-api/vytvoreni-zakladniho-dotazovaciho-php-formulare
+
+Our API doc
+https://hosting.eway-crm.com/cerpadla_vrchlabi/WcfService/Service.svc/help
+Health
+https://hosting.eway-crm.com/cerpadla_vrchlabi/
--- main.php
+++ main.php
@@ -9,41 +9,55 @@
 require "vendor/autoload.php";
 require_once "eway/eway.class.php";
 
-// read the configuration file
-$dataDir = getenv('KBC_DATADIR') . DIRECTORY_SEPARATOR;
-$configFile = $dataDir . 'config.json';
-$config = json_decode(file_get_contents($configFile), true);
+try {
+    // read the configuration file
+    $dataDir = getenv('KBC_DATADIR') . DIRECTORY_SEPARATOR;
+    $configFile = $dataDir . 'config.json';
+    $config = json_decode(file_get_contents($configFile), true);
 
-$webServiceAddress = $config['parameters']['webServiceAddress'];
-$username = $config['parameters']['username'];
-$password = $config['parameters']['password'];
-$passwordAlreadyEncrypted = $config['parameters']['passwordAlreadyEncrypted'];
-$dieOnItemConflict = $config['parameters']['dieOnItemConflict'];
+    $webServiceAddress = $config['parameters']['webServiceAddress'];
+    $username = $config['parameters']['username'];
+    $password = $config['parameters']['password'];
+    $passwordAlreadyEncrypted = $config['parameters']['passwordAlreadyEncrypted'];
+    $dieOnItemConflict = $config['parameters']['dieOnItemConflict'];
 
-// Create eWay API connector
-$connector = new eWayConnector($webServiceAddress, $username, $password, $passwordAlreadyEncrypted, $dieOnItemConflict);
+    echo "Host: " + $webServiceAddress;
+    echo "User: " + $username;
 
-
-// create output file and write header
-$outFile = new \Keboola\Csv\CsvFile(
-    $dataDir . 'out' . DIRECTORY_SEPARATOR . 'tables' . DIRECTORY_SEPARATOR . 'destination.csv'
-);
-$outFile->writeRow(['number', 'someText', 'double_number']);
-
-// read input file and write rows of output file
-$inFile = new Keboola\Csv\CsvFile($dataDir . 'in' . DIRECTORY_SEPARATOR . 'tables' . DIRECTORY_SEPARATOR . 'source.csv');
+    // Create eWay API connector
+    $connector = new eWayConnector($webServiceAddress, $username, $password, $passwordAlreadyEncrypted, $dieOnItemConflict);
 
 
-foreach ($inFile as $rowNum => $row) {
-    if ($rowNum == 0) {
-        // skip header
-        continue;
+    // create output file and write header
+    $outFile = new \Keboola\Csv\CsvFile(
+        $dataDir . 'out' . DIRECTORY_SEPARATOR . 'tables' . DIRECTORY_SEPARATOR . 'result.csv'
+    );
+    $outFile->writeRow(['id', 'cislo', 'nazev', 'ico']);
+
+    // read input file and write rows of output file
+    $inFile = new Keboola\Csv\CsvFile($dataDir . 'in' . DIRECTORY_SEPARATOR . 'tables' . DIRECTORY_SEPARATOR . 'zakazky.csv');
+
+
+    foreach ($inFile as $rowNum => $row) {
+        if ($rowNum == 0) {
+            // skip header
+            continue;
+        }
+        $outFile->writeRow([
+            $row[0],
+            $row[1],
+            $row[2],
+            $row[3]
+        ]);
     }
-    $outFile->writeRow([
-        $row[0],
-        $row[1],
-        $row[0] * 10 //$multiplier
-    ]);
+
+
+} catch (InvalidArgumentException $e) {
+    echo $e->getMessage();
+    exit(1);
+} catch (\Throwable $e) { // + $e
+    echo $e->getMessage();
+    exit(2);
 }
 
 
@@ -65,13 +79,3 @@ else
     echo "Unable to create new contact: {$result->Description}";
 }
 */
-
-try {
-
-} catch (InvalidArgumentException $e) {
-    echo $e->getMessage();
-    exit(1);
-} catch (\Throwable $e) { // + $e
-    echo $e->getMessage();
-    exit(2);
-}
